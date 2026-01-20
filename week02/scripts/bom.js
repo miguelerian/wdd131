11bc602a25bc0618eed1130c78a2754ca70ab8a2
miguelerian <mavalospriante@byupathway.edu>
2026-01-19 18:48:11 -0600

    learning activities

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,30 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
-li.appendChild(deleteButton);
-list.appendChild(li);
+
+button.addEventListener('click', function () {
+
+    if (input.value.trim() !== '') {
+
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        input.value = '';
+    }
+
+    input.focus();
+});
+
+list.addEventListener('click', function (e) {
+    if (e.target.tagName === 'BUTTON') {
+        list.removeChild(e.target.parentElement);
+        input.focus();
+    }
+});
