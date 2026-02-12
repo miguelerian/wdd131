e52fbb890e865285faa996df4219233b9f93e472
miguelerian <mavalospriante@byupathway.edu>
2026-02-12 12:42:41 -0600

    product form

---
+++ scripts/review.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let reviewCount = localStorage.getItem("reviewCount");
+
+    if (!reviewCount) {
+        reviewCount = 0;
+    }
+
+    reviewCount++;
+    localStorage.setItem("reviewCount", reviewCount);
+
+    document.getElementById("reviewCount").textContent = reviewCount;
+});
